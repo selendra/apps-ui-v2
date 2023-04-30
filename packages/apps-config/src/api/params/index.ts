a7fdede3df28167a16745c6b32d5a7c06d46dc4c
hangsiahong <honghangsia@gmail.com>
2023-04-30 08:43:21 +0700

    update

--- packages/apps-config/src/api/params/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017-2023 @polkadot/apps-config authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export * from './inflation';
-export * from './proposalThresholds';
-export * from './teleport';
-export * from './tracks';
