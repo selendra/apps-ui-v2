8bebc999e88f16c6336a3af2a675dfc19a69ce2d
SAN Vuthy <san.vuthy08@gmail.com>
2023-05-01 09:00:58 +0800

    update

--- packages/test-support/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2017-2023 @polkadot/test-support authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export * from './createApi.js';
-export * from './createAugmentedApi.js';
