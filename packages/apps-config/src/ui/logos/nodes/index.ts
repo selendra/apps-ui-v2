5a13a09bf7cd4284362f485231314e4aa4b14504
SAN Vuthy <san.vuthy08@gmail.com>
2023-08-27 09:34:47 +0700

    remove

--- packages/apps-config/src/ui/logos/nodes/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017-2023 @polkadot/apps authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// Do not edit. Auto-generated via node scripts/imgConvert.mjs
-
-export { nodesSelendraPNG } from './generated/selendraPNG.js';
-export { nodesSelendratestnetPNG } from './generated/selendratestnetPNG.js';
