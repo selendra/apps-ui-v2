4e21a4317cf26411785f547196704357edef64de
hangsiahong <honghangsia@gmail.com>
2024-08-01 10:34:51 +0700

    clean up

--- packages/react-api/src/util/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2023 @polkadot/react-api authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export { getEnvironment } from './getEnvironment.js';
-export { getHistoric } from './historic.js';
-export { intervalObservable } from './intervalObservable.js';
-export { isEqual } from './isEqual.js';
-export { triggerChange } from './triggerChange.js';
--- packages/react-components/src/styled.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2017-2023 @polkadot/react-components authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export { styled } from 'styled-components';
